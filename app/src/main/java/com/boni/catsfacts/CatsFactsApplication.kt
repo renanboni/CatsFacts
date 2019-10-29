7ac955362a6f1e070bcf9112cdbdeb11e9211e7d
renanboni <renan.boni@usp.br>
2019-10-28 23:03:35 -0300

    Added network module (koin)

--- app/src/main/java/com/boni/catsfacts/CatsFactsApplication.kt
+++ app/src/main/java/com/boni/catsfacts/CatsFactsApplication.kt
@@ -1,7 +1,25 @@
 package com.boni.catsfacts
 
 import android.app.Application
+import com.boni.catsfacts.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class CatsFactsApplication: Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidContext(this@CatsFactsApplication)
+            modules(
+                listOf(
+                    networkModule
+                )
+            )
+        }
+    }
 }
